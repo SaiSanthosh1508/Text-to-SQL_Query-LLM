dec0eff0eaf95e3627d17d1e441bdb56171712ae
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2025-02-24 15:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 from text_sql_pipeline import get_sql_query
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 st.title("📑 Convert Text to SQL-Query")
+
+question = st.text_input("Question",key=1,place_holder="Enter your text")
+st.subheader(question)
